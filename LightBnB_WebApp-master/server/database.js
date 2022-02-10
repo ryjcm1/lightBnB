81a5904e70386dd624898ac6225a370a13f8eeed
Jimmy Chuk <jimmychuk@outlook.com>
2022-02-10 12:43:49 -0500

    query refactor addProperty

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -29,15 +29,7 @@ const getUserWithEmail = function(email) {
     });
 
 
-  // for (const userId in users) {
-  //   user = users[userId];
-  //   if (user.email.toLowerCase() === email.toLowerCase()) {
-  //     break;
-  //   } else {
-  //     user = null;
-  //   }
-  // }
-  // return Promise.resolve(user);
+
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -175,9 +167,33 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+
+  const queryString = `INSERT 
+  INTO properties (owner_id, title, description, number_of_bedrooms, number_of_bathrooms, parking_spaces, cost_per_night, thumbnail_photo_url, cover_photo_url, street, country, city, province, post_code,  active)
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
+  RETURNING *;`;
+
+  values = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.number_of_bedrooms,
+    property.number_of_bathrooms,
+    property.parking_spaces,
+    property.cost_per_night*100,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.street,
+    property.country,
+    property.city,
+    property.province,
+    property.post_code,
+    true
+  ];
+  
+  return pool.query(queryString, values)
+  .then(res => res.rows[0])
+  .catch(err => console.log(err.message))
+
 }
 exports.addProperty = addProperty;
